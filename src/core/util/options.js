de353b1d79a79386169552e4c7a02aa837b032d9
Ryan <cn_ryan@outlook.com>
2022-06-21 17:39:46 +0800

    commit.

--- src/core/util/options.js
+++ src/core/util/options.js
@@ -21,6 +21,9 @@ import {
   isPlainObject
 } from 'shared/util'
 
+// start - 对config.optionMergeStrategies进行设置
+// strats.el/propsData/data
+
 /**
  * Option overwriting strategies are functions that handle
  * how to merge a parent option value and a child option
@@ -31,6 +34,9 @@ const strats = config.optionMergeStrategies
 /**
  * Options with restrictions
  */
+// 开发环境下设置strats.el、strats.propsData，实际上就是包裹默认strat，在没有提供vm时发出警告：
+// el选项或propsData选项只能在使用new操作符创建实例时可用。
+// 没有提供vm说明正在处理的是子组件的选项。
 if (process.env.NODE_ENV !== 'production') {
   strats.el = strats.propsData = function (parent, child, vm, key) {
     if (!vm) {
@@ -43,9 +49,12 @@ if (process.env.NODE_ENV !== 'production') {
   }
 }
 
+// start - 两个helper functions
+
 /**
  * Helper that recursively merges two data objects together.
  */
+// 只在mergeDataOrFn方法中被调用。
 function mergeData (to: Object, from: ?Object): Object {
   if (!from) return to
   let key, toVal, fromVal
@@ -76,19 +85,32 @@ function mergeData (to: Object, from: ?Object): Object {
 /**
  * Data
  */
+// 在strats.data中被调用；被赋给strats.provide。
+// 该方法一定会返回一个函数（parentVal/childVal/mergedDataFn/mergedInstanceDataFn）。
 export function mergeDataOrFn (
   parentVal: any,
   childVal: any,
   vm?: Component
 ): ?Function {
   if (!vm) {
+    // 没有提供vm，说明处理的是子组件的选项。
+
     // in a Vue.extend merge, both should be functions
+    // 没有提供vm时，childVal和parentVal只要存在，就一定是函数：
+    // 1. 二者均不存在，不会调用该方法，因此调用该方法时，两者必有其一；
+    // 2. childVal存在，则childVal（即子组件的options中的data）一定是函数。
+    // 3. Vue.options中没有data；因此parentVal（即父类的options中的data）如果存在，就一定来自某个组件类，从而一定是函数。
+
+    // childVal和parentVal至少有一个存在。
+    // 只有一个存在时，返回存在的值（值为函数）。
     if (!childVal) {
       return parentVal
     }
     if (!parentVal) {
       return childVal
     }
+
+    // 二者均存在时，返回函数mergedDataFn；调用该函数会调用mergeData函数并返回结果。
     // when parentVal & childVal are both present,
     // we need to return a function that returns the
     // merged result of both functions... no need to
@@ -101,6 +123,7 @@ export function mergeDataOrFn (
       )
     }
   } else {
+    // 提供了vm，说明是在使用new操作符创建实例时合并options，直接返回函数mergedInstanceDataFn。
     return function mergedInstanceDataFn () {
       // instance merge
       const instanceData = typeof childVal === 'function'
@@ -117,13 +140,20 @@ export function mergeDataOrFn (
     }
   }
 }
+// end - 两个helper functions
 
+// strats.data要么返回undefined（父类为Vue且子组件提供的data选项值不为函数），
+// 要么返回一个函数（调用mergeDataOrFn返回的结果）。
 strats.data = function (
   parentVal: any,
   childVal: any,
   vm?: Component
 ): ?Function {
+  // 没有提供vm，说明正在处理子组件的data选项。
   if (!vm) {
+    // 子组件提供了data选项，但选项值不为函数，则：
+    // 1. 在开发环境下发出警告：子组件的data选项值必须为函数。
+    // 2. 返回parentVal。
     if (childVal && typeof childVal !== 'function') {
       process.env.NODE_ENV !== 'production' && warn(
         'The "data" option should be a function ' +
@@ -134,9 +164,13 @@ strats.data = function (
 
       return parentVal
     }
+
+    // 不属于上面的特殊情况，则不传vm地 调用mergeDataOrFn并返回结果。
     return mergeDataOrFn(parentVal, childVal)
   }
 
+  // 提供了vm，说明正在处理的不是子组件的data选项，而是使用new操作符创建实例时的data选项。
+  // 传vm地 调用mergeDataOrFn并返回结果。
   return mergeDataOrFn(parentVal, childVal, vm)
 }
 
@@ -258,9 +292,12 @@ strats.computed = function (
 }
 strats.provide = mergeDataOrFn
 
+// end - 对config.optionMergeStrategies进行设置
+
 /**
  * Default strategy.
  */
+// 优先取child中的选项值，除非其为undefined。
 const defaultStrat = function (parentVal: any, childVal: any): any {
   return childVal === undefined
     ? parentVal
@@ -270,12 +307,14 @@ const defaultStrat = function (parentVal: any, childVal: any): any {
 /**
  * Validate component names
  */
+// 只在mergeOptions函数中被调用，用于检验组件命名是否合法，发出相关警告。
 function checkComponents (options: Object) {
   for (const key in options.components) {
     validateComponentName(key)
   }
 }
 
+// 在本文件中，只在checkComponents函数中被调用。
 export function validateComponentName (name: string) {
   if (!new RegExp(`^[a-zA-Z][\\-\\.0-9_${unicodeRegExp.source}]*$`).test(name)) {
     warn(
@@ -374,6 +413,7 @@ function normalizeDirectives (options: Object) {
   }
 }
 
+// 只在mergeAssets函数、strats.watch方法、strats.computed方法中被调用。
 function assertObjectType (name: string, value: any, vm: ?Component) {
   if (!isPlainObject(value)) {
     warn(
@@ -388,7 +428,7 @@ function assertObjectType (name: string, value: any, vm: ?Component) {
  * Merge two option objects into a new one.
  * Core utility used in both instantiation and inheritance.
  */
-// 该函数在实例化时（_init方法中）用到，在继承中（Vue.extend中）用到。
+// 该函数在实例化时（_init方法中）被调用，在继承中（Vue.extend中）被调用。
 export function mergeOptions (
   parent: Object,
   child: Object,
@@ -413,6 +453,9 @@ export function mergeOptions (
   // but only if it is a raw options object that isn't
   // the result of another mergeOptions call.
   // Only merged options has the _base property.
+  // 一开始时，只有Vue.options._base为true。
+  // 因此，如果child._base为true，说明child是某次mergeOptions调用的结果，
+  // 那么child.extends和child.mixins已经被合并过了，不再进行合并。
   if (!child._base) {
     if (child.extends) {
       parent = mergeOptions(parent, child.extends, vm)
@@ -428,6 +471,7 @@ export function mergeOptions (
   // 接下来，进行选项合并。
 
   const options = {}
+
   let key
   for (key in parent) {
     mergeField(key)
@@ -437,10 +481,15 @@ export function mergeOptions (
       mergeField(key)
     }
   }
+
+
   function mergeField (key) {
+    // 选取key相应的合并策略函数（starts中声明或取默认的defaultStrat）
     const strat = strats[key] || defaultStrat
+    // 调用 strat函数来合并key选项
     options[key] = strat(parent[key], child[key], vm, key)
   }
+
   return options
 }
 
